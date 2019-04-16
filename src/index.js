967455145450c8c170be766d01e29c04d3730970
yohei.yamana <fixeight@gmail.com>
2019-04-16 19:27:16 +0900

    initial

---
+++ src/index.js
@@ -0,0 +1,42 @@
+const ref = require('ref');
+const ffi = require('ffi');
+
+/**
+ * HTTP Cloud Function.
+ *
+ * @param {Object} req Cloud Function request context.
+ * @param {Object} res Cloud Function response context.
+ */
+exports.handler = (req, res) => {
+  const p_graal_isolatethread_t = ref.alloc(ref.refType(ref.types.void))
+
+  const libJava = ffi.Library(__dirname + '/libnativeimpl', {
+    graal_create_isolate: [
+      ref.types.int, [
+        ref.refType(ref.types.void),
+        ref.refType(ref.types.void),
+        ref.refType(ref.refType(ref.types.void))
+      ]],
+    Java_org_pkg_apinative_Native_hello: [
+      ref.types.CString,
+      [ref.refType(ref.types.void)]],
+    format_sql: [
+      ref.types.CString, [
+        ref.refType(ref.types.void),
+        ref.refType(ref.types.CString)
+      ]],
+  })
+
+  const rc = libJava.graal_create_isolate(ref.NULL, ref.NULL, p_graal_isolatethread_t)
+  if (rc !== 0) {
+    res.send('error on isolate creation or attach')
+    return;
+  }
+  if (req.method !== 'POST') {
+    const hello = libJava.Java_org_pkg_apinative_Native_hello(ref.deref(p_graal_isolatethread_t))
+    res.send(hello);
+  } else {
+    const rep = libJava.format_sql(ref.deref(p_graal_isolatethread_t), ref.allocCString(req.body.toString()))
+    res.send(rep);
+  }
+};
